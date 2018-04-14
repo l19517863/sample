2fc93cc51e35f6044c0ba91107e096477fd05334
lhz <l195178683@163.com>
2018-04-14 12:10:46 +0000

    Finish layout and routes

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -19,6 +19,5 @@ class StaticPagesController extends Controller
     public function about()
     {
         return view('static_pages/about');
-        return '关于页';
     }
 }
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,10 +5,17 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>@yield('title', '无标题传入')-非变量</title>
+  <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
+  <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
+    @include('layouts._header')
 
-  @yield('content')<!--  -->
-  @yield('content2')<!--  -->
-</body>
+    <div class="container">
+      <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div>
+  </body>
 </html>
